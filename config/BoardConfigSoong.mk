96cafadbf479bbdebad34beef0012ca6aa9e7083
danya2271 <danya2271@yandex.ru>
2023-07-23 13:23:44 +0500

    vendor: Enable Compose for SystemUI
    Change-Id: I1caa2ed057fbd06bd1ee6de5318ab6652fb23857

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -114,3 +114,5 @@ SOONG_CONFIG_lineageQcomVars_qcom_display_headers_namespace := vendor/qcom/opens
 else
 SOONG_CONFIG_lineageQcomVars_qcom_display_headers_namespace := $(QCOM_SOONG_NAMESPACE)/display
 endif
+
+SYSTEMUI_USE_COMPOSE = true
