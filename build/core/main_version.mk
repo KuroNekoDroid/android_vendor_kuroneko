d4964f73807b1ea9459986d0c659009c2b41518d
danya2271 <danya2271@yandex.ru>
2023-08-23 14:22:28 +0500

    Fix package name

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -6,20 +6,20 @@ endif
 
 # LineageOS System Version
 ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.kuroneko.version=$(LINEAGE_VERSION) \
-    ro.kuroneko.releasetype=$(LINEAGE_BUILDTYPE) \
+    ro.kuroneko.version=$(KURONEKO_VERSION) \
+    ro.kuroneko.releasetype=$(KURONEKO_BUILDTYPE) \
     ro.kuroneko.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
-    ro.modversion=$(LINEAGE_VERSION) \
+    ro.modversion=$(KURONEKO_VERSION) \
     ro.kuronekolegal.url=https://lineageos.org/legal
 
 # LineageOS Platform Display Version
 ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.kuroneko.display.version=$(LINEAGE_DISPLAY_VERSION)
+    ro.kuroneko.display.version=$(KURONEKO_DISPLAY_VERSION)
 
 # LineageOS Platform SDK Version
 ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.kuroneko.build.version.plat.sdk=$(LINEAGE_PLATFORM_SDK_VERSION)
+    ro.kuroneko.build.version.plat.sdk=$(KURONEKO_PLATFORM_SDK_VERSION)
 
 # LineageOS Platform Internal Version
 ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.kuroneko.build.version.plat.rev=$(LINEAGE_PLATFORM_REV)
+    ro.kuroneko.build.version.plat.rev=$(KURONEKO_PLATFORM_REV)
